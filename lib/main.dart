d32b79a985caa66480dfae4b1d1ae1ba5b2df973
SleepyWorker <dydwns5236@gmail.com>
2023-06-12 21:28:37 +0900

    아이콘 카드 만들기

--- lib/main.dart
+++ lib/main.dart
@@ -150,7 +150,90 @@ class App extends StatelessWidget{
                       bgColor: Color(0xFF1F2123),
                       textColor: Colors.white),
                 ],
-              )
+              ),
+              const SizedBox(
+                height: 100,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  const Text('Wallets',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 36,
+                          fontWeight: FontWeight.w600)),
+                  Text('View All',
+                      style: TextStyle(
+                          color: Colors.white.withOpacity(0.8), fontSize: 18)),
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Container(
+                //어떤 위젯이 오퍼플로우 했을 때, 카드와 같은 container로 하여금 어떻게 동작하게끔 할건지 알려주는 장치
+                clipBehavior: Clip.hardEdge,
+                decoration: BoxDecoration(
+                  color: const Color(0xFF1F2123),
+                  borderRadius: BorderRadius.circular(25),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(30),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            'Euro',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 32,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          Row(
+                            children: [
+                              const Text(
+                                '6 428',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20,
+                                ),
+                              ),
+                              const SizedBox(
+                                width: 5,
+                              ),
+                              Text(
+                                'EUR',
+                                style: TextStyle(
+                                  color: Colors.white.withOpacity(0.8),
+                                  fontSize: 20,
+                                ),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                      //아이콘의 크기 변경을 위한 scale
+                      Transform.scale(
+                        scale: 2.2,
+                        //아이콘의 x,y좌표 변경을 위한 translate
+                        child: Transform.translate(
+                          offset: Offset(-5,12),
+                          child: const Icon(Icons.euro_rounded,
+                              color: Colors.white, size: 88),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
             ],
           ),
         ),
